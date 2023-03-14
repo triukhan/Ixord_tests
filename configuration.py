8f9787bb1b2a76548853bca915e1a441d392c12d
Triukhan <tryukhan.d@gmail.com>
2023-03-14 12:24:54 +0200

    1.0 version for a tags

--- configuration.py
+++ configuration.py
@@ -1,1 +1,3 @@
 ENDPOINT = "https://stage.ixord.com/api/api/"
+ENDPOINT_TAG = "https://stage.ixord.com/api/api/tags/addTag?workspaceId="
+ENDPOINT_GTAG = "https://stage.ixord.com/api/api/tags/getTags?workspaceId="
